7547493bc753180efe848382f2476e7477d35182
abraham <piperidis3@yahoo.gr>
2016-12-17 23:08:41 +0200

    commit

--- Firedump/Firedump/Properties/Settings.Designer.cs
+++ Firedump/Firedump/Properties/Settings.Designer.cs
@@ -33,5 +33,17 @@ namespace Firedump.Properties {
                 return ((string)(this["firedumpdbConnectionString"]));
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string Setting {
+            get {
+                return ((string)(this["Setting"]));
+            }
+            set {
+                this["Setting"] = value;
+            }
+        }
     }
 }
